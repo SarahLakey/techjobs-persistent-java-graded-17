06cb123cd10b4a6564be96d83ef8dc354db02d7b
SarahLakey <136642286+SarahLakey@users.noreply.github.com>
2023-11-24 15:39:46 -0600

    Worked on first half of task 2 - all tests currently passing before uncommenting

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name too long!")
+    public String location;
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
